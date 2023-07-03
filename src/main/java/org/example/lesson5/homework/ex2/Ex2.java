ebcac6a684d7567810cb979cd9c741d8dbc48182
Dmitriy Kulakov <dk4git@outlook.com>
2023-07-03 23:44:56 +0300

    what is going on ....

--- src/main/java/org/example/lesson5/homework/ex2/Ex2.java
+++ src/main/java/org/example/lesson5/homework/ex2/Ex2.java
@@ -1,18 +1,28 @@
 package org.example.lesson5.homework.ex2;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.SortedMap;
+import java.util.*;
+
+import static java.util.stream.Collectors.*;
+import static java.util.Map.Entry.*;
 
 public class Ex2 {
     public static void main(String[] args) {
-        printMap(getMap());
-        Map<String,Integer> forPrint=getCountOfName(getMap());
-        printMap2(forPrint);
-        forPrint.
+        Map<String,Integer> resultMap=getCountOfName(getMap());
+        System.out.println("resultMap = " + resultMap);
+        System.out.println("getSorted(resultMap) = " + getSorted(resultMap));
+//        List<Integer> resultList = reverseList(resultMap);
+//        System.out.println("resultList = " + resultList);
+//        System.out.println("getSorted(resultMap,resultList) = " + getSorted(resultMap, resultList));
+
+
     }
 
+    /**
+     * @apiNote принимает Map <String, String> и возвращает новый Map <String, Integer>.
+     * Подсчитывает, сколько раз повторялось значение в переданном Map.
+     * @param map <String, String>
+     * @return Map<String, Integer>
+     */
     public static Map<String, Integer> getCountOfName(Map<String, String> map) {
         Map<String, Integer> newMap = new HashMap<>();
         for (String i : map.keySet()) {
@@ -28,6 +38,91 @@ public class Ex2 {
 
         return newMap;
     }
+
+//    public static Map<String, Integer> getSorted (Map<String, Integer> map) {
+//        NavigableMap<String,Integer> test2 = new TreeMap<>(map);
+//        test2.navigableKeySet();
+//    }
+
+    public static Map<String, Integer> getSorted2 (Map<String, Integer> map){
+        Map<String, Integer> newMap = new HashMap<>();
+        for (String s : map.keySet()) {
+            Integer count = map.get(s);
+            for (String s1 : map.keySet()) {
+                if (map.get(s1)>count){
+
+            }
+
+
+            }
+        }
+    }
+
+
+    /**
+     * @apiNote сортирует Map по убыванию значения.
+     * @param map Map<String, Integer>
+     * @return отсортированный Map<String, Integer>
+     */
+//    public static Map<String, Integer> getSorted (Map<String, Integer> map, List<Integer>tempList) {
+//        Map<String, Integer> newMap = new HashMap<>();
+//        for (Integer integer : tempList) {
+//            for (String s : map.keySet()) {
+//                System.out.println("s = " + s);
+//                if (map.get(s).equals(integer)) {
+//                    System.out.println("map.get(s) = " + map.get(s));
+//                    newMap.put(s, map.get(s));
+//                    System.out.println("newMap = " + newMap);
+//                }
+//
+//            }
+//
+//        }
+//
+//        return newMap;
+//    }
+
+
+
+//    public static List<Integer> reverseList(Map<String, Integer> map){
+//        SortedMap<String,Integer> test = new TreeMap<>(map);
+//        NavigableMap<String,Integer> test2 = new TreeMap<>(map);
+//        test2.descendingMap();
+//        Map<String, Integer> test3 = test2.descendingMap();
+//
+//
+//        List<Integer> tempList = new ArrayList<>(map.values());
+//        tempList.sort(Collections.reverseOrder());
+//        System.out.println("tempList = " + tempList);
+//        return tempList;
+//    }
+
+
+    /**
+     * @apiNote Распечатывает Map <String,String>
+     * @param map <String,String>
+     */
+    public static void printMap(Map<String, String> map) {
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            String key = pair.getKey();
+            String value = pair.getValue();
+            System.out.println(key + ":" + value);
+        }
+    }
+
+    /**
+     * @apiNote распечатывает Map <String, Integer>
+     * @param map <String, Integer>
+     */
+    public static void printMap2(Map<String, Integer> map) {
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            String key = pair.getKey();
+            Integer value = pair.getValue();
+            System.out.println(key + ":" + value);
+        }
+    }
+
+
 
     /**
      * @return записаный Map
@@ -53,30 +148,5 @@ public class Ex2 {
         namesMap.put("Ежов", "Иван");
         namesMap.put("Мечников", "Иван");
         return namesMap;
-    }
-
-    /**
-     * @param map <String,String>
-     * @apiNote Распечатывает Map <String,String>
-     */
-    public static void printMap(Map<String, String> map) {
-        for (Map.Entry<String, String> pair : map.entrySet()) {
-            String key = pair.getKey();
-            String value = pair.getValue();
-            System.out.println(key + ":" + value);
-        }
-    }
-
-    public static void printMap2(Map<String, Integer> map) {
-        for (Map.Entry<String, Integer> pair : map.entrySet()) {
-            String key = pair.getKey();
-            Integer value = pair.getValue();
-            System.out.println(key + ":" + value);
-        }
-    }
-
-    public static Map<String,Integer> getSortedMap (Map<String, Integer> map){
-        return map;
-
     }
 }
