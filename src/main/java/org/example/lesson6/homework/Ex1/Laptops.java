b5bcad59c002057dc8805248180bbdf9aee1ac50
Dmitriy Kulakov <dk4git@outlook.com>
2023-07-07 19:29:16 +0300

    HM6 is almost done

--- src/main/java/org/example/lesson6/homework/Ex1/Laptops.java
+++ src/main/java/org/example/lesson6/homework/Ex1/Laptops.java
@@ -1,52 +1,94 @@
 package org.example.lesson6.homework.Ex1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Laptops {
 
-    public ArrayList<Laptop> getLaptopOZU(ArrayList<Laptop> listOfLaptops, int numbOZU) {
+    /**
+     * @apiNote Переберает массив, считывает значение введенное с клавиатуры. Значение - желаемый вид фильтрации.
+     * @param listOfLaptops массив Ноутбуков.
+     * @return результат фильтрации по OZU
+     */
+    public ArrayList<Laptop> getLaptopOZU(ArrayList<Laptop> listOfLaptops) {
+        System.out.println("Enter the OZU of laptop:");
+        Scanner scanner = new Scanner(System.in);
+        Integer fromKeyBord = scanner.nextInt();
         ArrayList<Laptop> newListOZU = new ArrayList<>();
         for (int i = 0; i < listOfLaptops.size(); i++) {
-            if (listOfLaptops.get(i).getOZU() == numbOZU) {
+            if (listOfLaptops.get(i).getOZU() == fromKeyBord) {
                 newListOZU.add(listOfLaptops.get(i));
             }
         }
         return newListOZU;
     }
 
-    public ArrayList<Laptop> getLaptopHDD(ArrayList<Laptop> listOfLaptops, int numbHDD) {
-//        Map <Integer, Laptop> newMap = new HashMap<>();
+    /**
+     * @apiNote Переберает массив, считывает значение введенное с клавиатуры. Значение - желаемый вид фильтрации.
+     * @param listOfLaptops массив Ноутбуков.
+     * @return езультат фильтрации по HDD
+     */
+    public ArrayList<Laptop> getLaptopHDD(ArrayList<Laptop> listOfLaptops) {
+        System.out.println("Enter the HDD of laptop:");
+        Scanner scanner = new Scanner(System.in);
+        Integer fromKeyBord = scanner.nextInt();
         ArrayList<Laptop> newListHDD = new ArrayList<>();
         for (int i = 0; i < listOfLaptops.size(); i++) {
-            if (listOfLaptops.get(i).getHDD() == numbHDD) {
+            if (listOfLaptops.get(i).getHDD().equals(fromKeyBord)) {
                 newListHDD.add(listOfLaptops.get(i));
             }
         }
         return newListHDD;
     }
-    public ArrayList<Laptop> getLaptopGP(ArrayList<Laptop> listOfLaptops, int numbGP) {
+
+    /**
+     * @apiNote Переберает массив, считывает значение введенное с клавиатуры. Значение - желаемый вид фильтрации.
+     * @param listOfLaptops массив Ноутбуков.
+     * @return езультат фильтрации по GP
+     */
+    public ArrayList<Laptop> getLaptopGP(ArrayList<Laptop> listOfLaptops) {
+        System.out.println("Enter the GP of laptop:");
+        Scanner scanner = new Scanner(System.in);
+        Integer fromKeyBord = scanner.nextInt();
         ArrayList<Laptop> newListGP = new ArrayList<>();
         for (int i = 0; i < listOfLaptops.size(); i++) {
-            if (listOfLaptops.get(i).getGP() == numbGP) {
+            if (listOfLaptops.get(i).getGP().equals(fromKeyBord)) {
                 newListGP.add(listOfLaptops.get(i));
             }
         }
         return newListGP;
 }
-    public ArrayList<Laptop> getLaptopYear(ArrayList<Laptop> listOfLaptops, int numbYear) {
+
+    /**
+     * @apiNote Переберает массив, считывает значение введенное с клавиатуры. Значение - желаемый вид фильтрации.
+     * @param listOfLaptops массив Ноутбуков.
+     * @return езультат фильтрации по Year
+     */
+    public ArrayList<Laptop> getLaptopYear(ArrayList<Laptop> listOfLaptops) {
+        System.out.println("Enter the Year of laptop:");
+        Scanner scanner = new Scanner(System.in);
+        Integer fromKeyBord = scanner.nextInt();
         ArrayList<Laptop> newListYear = new ArrayList<>();
         for (int i = 0; i < listOfLaptops.size(); i++) {
-            if (listOfLaptops.get(i).getYear() == numbYear) {
+            if (listOfLaptops.get(i).getYear().equals(fromKeyBord)) {
                 newListYear.add(listOfLaptops.get(i));
             }
         }
         return newListYear;
     }
 
-    public ArrayList<Laptop> getLaptopName(ArrayList<Laptop> listOfLaptops, String numbName) {
+    /**
+     * @apiNote Переберает массив, считывает значение введенное с клавиатуры. Значение - желаемый вид фильтрации.
+     * @param listOfLaptops массив Ноутбуков.
+     * @return езультат фильтрации по Name
+     */
+    public ArrayList<Laptop> getLaptopName(ArrayList<Laptop> listOfLaptops) {
+        System.out.println("Enter the Name of laptop:");
+        Scanner scanner = new Scanner(System.in);
+        String fromKeyBord = scanner.next();
         ArrayList<Laptop> newListName = new ArrayList<>();
         for (int i = 0; i < listOfLaptops.size(); i++) {
-            if (listOfLaptops.get(i).getName().contains(numbName)) {
+            if (listOfLaptops.get(i).getName().contains(fromKeyBord)) {
 
                 newListName.add(listOfLaptops.get(i));
             }
@@ -54,25 +96,47 @@ public class Laptops {
 //        System.out.println("newListName = " + newListName);
         return newListName;
     }
-    public ArrayList<Laptop> getLaptopColor(ArrayList<Laptop> listOfLaptops, String color) {
+
+    /**
+     * @apiNote Переберает массив, считывает значение введенное с клавиатуры. Значение - желаемый вид фильтрации.
+     * @param listOfLaptops массив Ноутбуков.
+     * @return езультат фильтрации по Color;
+     */
+    public ArrayList<Laptop> getLaptopColor(ArrayList<Laptop> listOfLaptops) {
+        System.out.println("Enter the color of laptop:");
+        Scanner scanner = new Scanner(System.in);
+        String fromKeyBord = scanner.next();
         ArrayList<Laptop> newListColor = new ArrayList<>();
         for (int i = 0; i < listOfLaptops.size(); i++) {
-            if (listOfLaptops.get(i).getColor().contains(color)) {
+            if (listOfLaptops.get(i).getColor().contains(fromKeyBord)) {
                 newListColor.add(listOfLaptops.get(i));
             }
         }
         return newListColor;
     }
-    public ArrayList<Laptop> getLaptopOS(ArrayList<Laptop> listOfLaptops, String oS) {
+
+    /**
+     * @apiNote Переберает массив, считывает значение введенное с клавиатуры. Значение - желаемый вид фильтрации.
+     * @param listOfLaptops массив Ноутбуков.
+     * @return езультат фильтрации по OS;
+     */
+    public ArrayList<Laptop> getLaptopOS(ArrayList<Laptop> listOfLaptops) {
+        System.out.println("Enter the OS of laptop:");
+        Scanner scanner = new Scanner(System.in);
+        String fromKeyBord = scanner.next();
         ArrayList<Laptop> newListOS = new ArrayList<>();
         for (int i = 0; i < listOfLaptops.size(); i++) {
-            if (listOfLaptops.get(i).getOS().contains(oS)) {
+            if (listOfLaptops.get(i).getOS().contains(fromKeyBord)) {
                 newListOS.add(listOfLaptops.get(i));
             }
         }
         return newListOS;
     }
 
+    /**
+     * @apiNote Список ноутбуков.
+     * @return
+     */
     public ArrayList<Laptop> listOfLaptops(){
         ArrayList<Laptop> laptops = new ArrayList<>();
         laptops.add( new Laptop("Nosy",128,500,256,1999,"Windows","yellow"));
--- src/main/java/org/example/lesson6/homework/Ex1/Main.java
+++ src/main/java/org/example/lesson6/homework/Ex1/Main.java
@@ -7,24 +7,67 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Напишите название ");
-        String number = scanner.next();
-        Laptops getName = new Laptops();
-        ArrayList<Laptop> newList = getName.getLaptopName(getName.listOfLaptops(),number);
-        System.out.println("newList = " + newList);
-//        getName.getLaptopName(getName.listOfLaptops(),number);
+        Laptops getMeth = new Laptops();
+        Integer forGet = getNum();
 
-//        Laptop nosy = new Laptop("",128,500,256,1999,"Windows","yellow");
-//        Laptop  = new Laptop("ellpa",256,500,512,2000,"Apple","red");
-//        Laptop Ssusa = new Laptop("Ssusa",512,256,64,1995,"Android","black");
-//        Laptop Sera = new Laptop("Sera",256,500,512,2000,"Windows","green");
-//        Laptop SeraViper  = new Laptop("SeraViper",128,500,256,1999,"Windows","yellow");
-//        Laptop SsusaQwerty = new Laptop("SsusaQwerty",256,500,512,2000,"Apple","white");
-//        Laptop ellpaAlfa = new Laptop("ellpaAlfa",512,256,64,1995,"Android","black");
-//        Laptop nosyPrime = new Laptop("nosyPrime",256,500,512,2000,"Windows","green");
 
     }
 
+    /**
+     * @apiNote создает Мар с перечнем доступных для фильтрации
+     * @return Мар
+     */
+    public static Map<Integer,ArrayList> setMapForChoice(){
+        Laptops ge = new Laptops();
+        Map<Integer,ArrayList> mapForFilter = new HashMap<>();
+        mapForFilter.put(1, ge.getLaptopName(ge.listOfLaptops()));
+//        mapForFilter.put(2, "OZU");
+//        mapForFilter.put(3, "HDD");
+//        mapForFilter.put(4, "GP");
+//        mapForFilter.put(5, "Year");
+//        mapForFilter.put(6, "OS");
+//        mapForFilter.put(7, "Color");
+        return mapForFilter;
+    }
+
+
+    /**
+     * @apiNote Метод получает считывает значение с клавиатуры и по полученному значениею возвращает нужный
+     * метод фильтрации
+     * @param map
+     * @return
+     */
+    public static ArrayList<Laptop> getChoiceForFilter(Map<Integer, ArrayList> map) {
+        ArrayList<Laptop> result = new ArrayList<>();
+         System.out.println("""
+                Enter the number of filer what you want to get:\s
+                 1 - Name;
+                 2 - OZU;
+                 3 - HDD;
+                 4 - GP;
+                 5 - Year;
+                 6 - OS;
+                 7 - Color.""");
+        Scanner scanner = new Scanner(System.in);
+        Integer numberForChoice = scanner.nextInt();
+        for (Integer integer : map.keySet()) {
+            if (numberForChoice.equals(integer)) {
+                result.add(map.get(integer));
+
+
+            }
+        }return result;
+    }
+
+    /**
+     * @apiNote Считывет значение введённое с клавиатуры.
+     * @return
+     */
+    public static Integer getNum(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        Integer num = scanner.nextInt();
+        return  num;
+    }
 }
