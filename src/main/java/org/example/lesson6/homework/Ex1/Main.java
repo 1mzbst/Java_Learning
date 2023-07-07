e2b7bcbea3e59b064a34c269d603dd8916e66ab3
Dmitriy Kulakov <dk4git@outlook.com>
2023-07-07 19:53:33 +0300

    HW6 is not done but iam glad that know how

--- src/main/java/org/example/lesson6/homework/Ex1/Main.java
+++ src/main/java/org/example/lesson6/homework/Ex1/Main.java
@@ -7,40 +7,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Laptops getMeth = new Laptops();
-        Integer forGet = getNum();
+//        Scanner scanner = new Scanner(System.in);
+//        Laptops getMeth = new Laptops();
+//        Integer forGet = getNum();
 
+        Map<Integer, ArrayList> sMap = setMapForChoice();
+        System.out.println("sMap.get(1) = " + sMap.get(1));
+//        System.out.println("getChoiceForFilter(sMap) = " + getChoiceForFilter(sMap));
 
     }
 
     /**
-     * @apiNote создает Мар с перечнем доступных для фильтрации
+     *@apiNote Дошло как это работает только в процессе вызова, оказывается он вызывает их поочереди,
+     * что конечно очевидно. Здесь надо указать название сегментов фильтрации, а затем как в методе ниже,
+     * доделать реализацию : Какой фильтр хотите выбрать? Если то-то тогда вызывать метод который указал в этом
+     * методе.
+     *
      * @return Мар
+     *
      */
-    public static Map<Integer,ArrayList> setMapForChoice(){
+    public static Map<Integer, ArrayList> setMapForChoice() {
         Laptops ge = new Laptops();
-        Map<Integer,ArrayList> mapForFilter = new HashMap<>();
+        Map<Integer, ArrayList> mapForFilter = new HashMap<>();
         mapForFilter.put(1, ge.getLaptopName(ge.listOfLaptops()));
-//        mapForFilter.put(2, "OZU");
-//        mapForFilter.put(3, "HDD");
-//        mapForFilter.put(4, "GP");
-//        mapForFilter.put(5, "Year");
-//        mapForFilter.put(6, "OS");
-//        mapForFilter.put(7, "Color");
+//        mapForFilter.put(2, ge.getLaptopOZU(ge.listOfLaptops()));
+//        mapForFilter.put(3, ge.getLaptopHDD(ge.listOfLaptops()));
+//        mapForFilter.put(4, ge.getLaptopGP(ge.listOfLaptops()));
+//        mapForFilter.put(5, ge.getLaptopYear(ge.listOfLaptops()));
+//        mapForFilter.put(6, ge.getLaptopOS(ge.listOfLaptops()));
+//        mapForFilter.put(7, ge.getLaptopColor(ge.listOfLaptops()));
         return mapForFilter;
     }
 
 
     /**
-     * @apiNote Метод получает считывает значение с клавиатуры и по полученному значениею возвращает нужный
-     * метод фильтрации
      * @param map
      * @return
+     * @apiNote Метод получает считывает значение с клавиатуры и по полученному значениею возвращает нужный
+     * метод фильтрации
      */
-    public static ArrayList<Laptop> getChoiceForFilter(Map<Integer, ArrayList> map) {
-        ArrayList<Laptop> result = new ArrayList<>();
-         System.out.println("""
+    public static ArrayList<ArrayList> getChoiceForFilter(Map<Integer, ArrayList> map) {
+        ArrayList<ArrayList> result = new ArrayList<>();
+        System.out.println("""
                 Enter the number of filer what you want to get:\s
                  1 - Name;
                  2 - OZU;
@@ -57,17 +65,18 @@ public class Main {
 
 
             }
-        }return result;
+        }
+        return result;
     }
 
     /**
-     * @apiNote Считывет значение введённое с клавиатуры.
      * @return
+     * @apiNote Считывет значение введённое с клавиатуры.
      */
-    public static Integer getNum(){
+    public static Integer getNum() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number: ");
         Integer num = scanner.nextInt();
-        return  num;
+        return num;
     }
 }
